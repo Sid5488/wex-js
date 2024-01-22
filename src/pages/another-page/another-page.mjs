99116cb31374b0b548bae0a9bf0d0bcf99af3163
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2024-01-21 22:27:51 -0300

    feat: Styling home page

--- src/pages/another-page/another-page.mjs
+++
@@ -1,9 +0,0 @@
-import { BaseHTMLComponent } from "../../../lib/src/base-html-component.mjs";
-
-class AnotherPage extends BaseHTMLComponent {
-	constructor() {
-		super("home-app", [], false);
-	}
-}
-
-export { AnotherPage };
