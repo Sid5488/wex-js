56e616feb305432abe411f2606c07132bf186899
Guilherme Sousa <guilhermenoobsaibot@gmail.com>
2024-01-22 02:44:50 -0300

    feature: waiting for resources to be loaded

--- src/pages/home-app/home-app.mjs
+++ src/pages/home-app/home-app.mjs
@@ -1,8 +1,33 @@
 import { BaseHTMLComponent } from "../../../lib/src/base-html-component.mjs";
+import { Observable } from "../../../lib/src/observable/observables.mjs";
 
 class HomeApp extends BaseHTMLComponent {
 	constructor() {
 		super("home-app", [], false);
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+		
+		this.getValueInput();
+	}
+
+	getValueInput() {
+		const inputObservable = new Observable();
+		const input = this.shadowRoot.getElementById("name");
+
+		input.addEventListener("input", (event) => {
+			inputObservable._value = event.target.value;
+			this.name = inputObservable._value;
+
+			this.showValue();
+		});
+	}
+
+	showValue() {
+		const value = this.shadowRoot.getElementById("value");
+
+		value.innerText = this.name;
 	}
 }
 
